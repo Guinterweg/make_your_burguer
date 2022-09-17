41788780cd05410d8c9db640675d5c411bea59ae
Guinter <guiwegner@gmail.com>
2022-09-17 13:55:45 -0300

    up

---
+++ src/components/Navbar.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="nav">
+    <router-link to="/">
+      <img :src="logo" :alt="alt" id="logo" />
+    </router-link>
+    <router-link to="/"> Home </router-link>
+    <router-link to="/pedidos"> pedidos </router-link>
+  </div>
+</template>
+<script>
+export default {
+  name: 'Navbar',
+  props: ['logo', 'alt']
+}
+</script>
+<style scoped></style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="home"></div>
+  <h1>Home</h1>
+</template>
+
+<script></script>
